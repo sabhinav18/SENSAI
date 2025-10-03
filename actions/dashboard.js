bb73aaa666c717871109b832f40af172a2b7e91a
sabhinav18 <156115964+sabhinav18@users.noreply.github.com>
2025-10-03 10:41:18 +0530

    Update dashboard.js

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -7,7 +7,9 @@ import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "models/gemini-2.5-flash" });
+
 
 export const generateAIInsights = async (industry) => {
   const prompt = `
